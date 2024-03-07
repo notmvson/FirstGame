f01c167218210befcd6806f05c8650df2aebd056
Gunnyyyy <135295777+Gunnyyyy@users.noreply.github.com>
2024-03-06 19:07:21 -0800

    Sprite sheet added with game object changes

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-// Definitional code
-
 // Create the canvas
 let canvas = document.createElement("canvas");
 let ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
+canvas.width = 700;
+canvas.height = 600;
 document.body.appendChild(canvas);
 
 // Background image
@@ -15,40 +13,38 @@ bgImage.onload = function() {
 };
 bgImage.src = "images/background.png";
 
-// Hero image
-let heroReady = false;
-let heroImage = new Image();
-heroImage.onload = function() {
-    heroReady = true;
+// Meat image
+let meatReady = false;
+let meatImage = new Image();
+meatImage.onload = function() {
+    meatReady = true;
 };
-heroImage.src = "images/hero.png";
+meatImage.src = "images/meat.png";
 
-// Monster image
-let monsterReady = false;
-let monsterImage = new Image();
-monsterImage.onload = function() {
-    monsterReady = true;
+// Dog image
+let dogReady = false;
+let dogImage = new Image();
+dogImage.onload = function() {
+    dogReady = true;
 };
-monsterImage.src = "images/monster.png";
+dogImage.src = "images/dog.png";
 
 // Game objects
-var hero = {
-    speed: 256, // movement in pixels per second
-    x: 0, 
-    y: 0
-};
-
-var monster = {
+var dog = {
+    speed: 256,
     x: 0,
     y: 0
 };
 
-var monstersCaught = 0;
+var meat = {
+    x: 0,
+    y: 0
+};
 
-// Start of code that actually runs ======================================
+var meatCaught = 0;
 
-// Handle keyboard controls
-var keysDown = {}; 
+// Keyboard controls
+var keysDown = {};
 
 addEventListener("keydown", function(e) {
     keysDown[e.keyCode] = true;
@@ -58,71 +54,120 @@ addEventListener("keyup", function(e) {
     delete keysDown[e.keyCode];
 }, false);
 
+// Animation variables
+var trackDown = 0;
+var trackRight = 2;
+var trackUp = 3;
+var trackLeft = 1;
+var rows = 4;
+var cols = 8;
+var spriteWidth = 512;
+var spriteHeight = 246;
+var width = spriteWidth / cols;
+var height = spriteHeight / rows;
+var curXFrame = 0;
+var frameCount = 1;
+var srcX = 0;
+var srcY = 0;
+var left = false;
+var right = false;
+var up = false;
+var down = false;
+
 // Update game objects
 var update = function(modifier) {
-    if(38 in keysDown && hero.y > 32+0) { // Up
-        hero.y -= hero.speed * modifier;
-    }
-    if(40 in keysDown && hero.y < canvas.height - (64+0)) { // Down
-        hero.y += hero.speed * modifier;
-    }
-    if(37 in keysDown && hero.x > 32+0) { // Left
-        hero.x -= hero.speed * modifier;
-    }
-    if(39 in keysDown && hero.x < canvas.width - (64+0)) { // Right
-        hero.x += hero.speed * modifier;
+    ctx.clearRect(dog.x, dog.y, width, height);
+    left = false;
+    right = false;
+    up = false;
+    down = false;
+
+    if (37 in keysDown && dog.x > (32)) { // Left key
+        dog.x -= dog.speed * modifier;
+        left = true;
     }
 
-    if (
-        hero.x <= (monster.x + 32)
-        && monster.x <= (hero.x + 32)
-        && hero.y <= (monster.y + 32)
-        && monster.y <= (hero.y + 32)
-    ) {
-        ++monstersCaught; // Keeps track of score
-        reset();
+    if (39 in keysDown && dog.x < canvas.width - (96)) { // Right key
+        dog.x += dog.speed * modifier;
+        right = true;
     }
+
+    if (38 in keysDown && dog.y > (32)) { // Up key
+        dog.y -= dog.speed * modifier;
+        up = true;
+    }
+
+    if (40 in keysDown && dog.y < canvas.height - (96)) { // Down key
+        dog.y += dog.speed * modifier;
+        down = true;
+
+        if (
+            dog.x <= (meat.x + 32)
+            && meat.x <= (dog.x + 32)
+            && dog.y <= (meat.y + 32)
+            && meat.y <= (dog.y + 32)
+        ) {
+            ++meatCaught; // Keeps track of score
+            reset();
+        }
+    }
+
+    // Determines appropriate row for animation
+    if (left) {
+        srcY = trackLeft * height;
+    } else if (right) {
+        srcY = trackRight * height;
+    } else if (up) {
+        srcY = trackUp * height;
+    } else if (down) {
+        srcY = trackDown * height;
+    }
+
+    // Updates frame count for animation
+    curXFrame = ++curXFrame % frameCount;
+    srcX = curXFrame * width;
 };
 
-// Draw everything in the main render function
+// Render function
 var render = function() {
-    if(bgReady) {
+    if (bgReady) {
         ctx.drawImage(bgImage, 0, 0);
     }
 
-    if(heroReady) {
-        ctx.drawImage(heroImage, hero.x, hero.y);
+    if (dogReady) {
+        ctx.drawImage(dogImage, srcX, srcY, width, height, dog.x, dog.y, width, height);
     }
-    
-    if(monsterReady) {
-        ctx.drawImage(monsterImage, monster.x, monster.y);
+
+    if (meatReady) {
+        ctx.drawImage(meatImage, meat.x, meat.y);
     }
 
     ctx.fillSytle = "rgb(250, 250, 250)";
-    ctx.font = "24px Helvetica";
+    ctx.font = "28px Helvetica";
     ctx.textAlign = "left";
     ctx.textBaseline = "top";
-    ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+    ctx.fillText("Meat Count: " + meatCaught, 32, 32);
 };
 
-// The main game loop
-var main = function() { 
+// Main game loop
+var main = function() {
     var now = Date.now();
     var delta = now - then;
-    update(delta/1000);
+    update(delta / 1000);
     render();
     then = now;
     requestAnimationFrame(main);
 };
 
 var reset = function() {
-    hero.x = (canvas.width / 2) - 16;
-    hero.y = (canvas.height / 2) - 16;
+    dog.x = (canvas.width / 2) - 16;
+    dog.y = (canvas.height / 2) - 16;
 
-    monster.x = 32 + (Math.random() * (canvas.width - 96));
-    monster.y = 32 + (Math.random() * (canvas.height - 96));
+    meat.x = 32 + (Math.random() * (canvas.width - 96));
+    meat.y = 32 + (Math.random() * (canvas.height - 96));
 };
 
+// Initialization
 var then = Date.now();
 reset();
-main(); 
+main();
